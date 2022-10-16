1c6fcf3c57ec61f69970ae8a1284f76d09b6326e
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-16 19:15:43 -0300

    feat: :sparkles: add a route to StudentList page

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from '../assets/styles/globalStyle';
 import Home from '../pages/Home';
-
+import StudentsList from '../pages/StudentsList';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/students' element={<StudentsList />} />
+        </Routes>
+      </Router>
+    </>
+
   )
 
 }
