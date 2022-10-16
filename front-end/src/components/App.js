08ee50b62827da5d0973dc45d75abaedb9e14648
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-16 09:41:34 -0300

    refactor: :recycle: takeout async term of useEffect function and create a function asyncronus inside useEffect block

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -4,19 +4,22 @@ import { useEffect, useState } from 'react';
 function App() {
   const [student, setStudent] = useState(null);
 
-  useEffect(async () => {
-    const API_URL = process.env.REACT_APP_BASE_URL;
-    try {
-      const response = await axios.get(`${API_URL}/students/random`);
-      const student = response.data;
-      if (!student) {
-        alert('Xablau! Não há estudantes cadastrados para o sorteio!');
-      } else {
-        setStudent(student);
+  useEffect(() => {
+    async function fetchData() {
+      const API_URL = process.env.REACT_APP_BASE_URL;
+      try {
+        const response = await axios.get(`${API_URL}/students/random`);
+        const student = response.data;
+        if (!student) {
+          alert('Xablau! Não há estudantes cadastrados para o sorteio!');
+        } else {
+          setStudent(student);
+        }
+      } catch (error) {
+        alert('Não foi possível realizar o sorteio!');
       }
-    } catch (error) {
-      alert('Não foi possível realizar o sorteio!');
     }
+    fetchData()
   }, []);
 
   return student ? <h1>{student.name}</h1> : 'Carregando...';
