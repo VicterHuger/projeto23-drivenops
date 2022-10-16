53720c48056fee2575ad79303626deb106bbafd6
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-16 19:12:58 -0300

    feat: :sparkles: better style and add option to see all students or reload a new random student

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -1,19 +1,37 @@
+import { Link } from "react-router-dom";
+import { Container } from "../../assets/styles/Container";
+import Loading from "../../components/Loading";
 import useRandomStudent from "../../hooks/api/useRandomStudent";
 
 export default function Home() {
 
-  const { loadingGettingRandomStudent, errorGettingRandomStudent, randomStudent } = useRandomStudent();
+  const { loadingGettingRandomStudent, errorGettingRandomStudent, randomStudent, listRandomStudent } = useRandomStudent();
 
 
   if (errorGettingRandomStudent) return (<h1>Não foi possível realizar o sorteio!</h1>);
 
   if ((loadingGettingRandomStudent && !randomStudent)) {
-    return (<h1>Carregando...</h1>);
+    return (
+      <Container>
+        <Loading />
+      </Container>);
   }
 
   return (
     <>
-      {!randomStudent ? <h1>Xablau! Não há estudantes cadastrados para o sorteio!</h1> : <h1>{randomStudent.name} </h1>}
+      {!randomStudent ?
+        <Container>
+          <h1>Xablau! Não há estudantes cadastrados para o sorteio!</h1>
+        </Container> :
+        <Container>
+          <button onClick={() => listRandomStudent()}>
+            Selecionar Aluno aleatóriamente
+          </button>
+          <h1>{randomStudent.name} </h1>
+          <Link to='/students'>
+            Ver toda a lista de estudantes
+          </Link>
+        </Container>}
     </>
 
   )
