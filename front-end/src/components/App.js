851b369ec43554c6e6973736edcd7282f47666e9
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-16 19:15:08 -0300

    feat: :lipstick: add a global style to project

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GlobalStyle } from '../assets/styles/globalStyle';
 import Home from '../pages/Home';
 
 
 function App() {
   return (
+    <>
+      <GlobalStyle />
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
