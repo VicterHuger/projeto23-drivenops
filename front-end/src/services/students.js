e755a05266a9eb08b6c12c601cbc9fbe6568d5b3
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-16 16:42:42 -0300

    fix: :bug: correct end point in axios routes

--- front-end/src/services/students.js
+++ front-end/src/services/students.js
@@ -1,16 +1,16 @@
 import api from './api';
 
 export async function getStudents() {
-    const { data } = await api.get('/students');
+    const { data } = await api.get('students');
     return data;
 }
 
 export async function createStudents(body) {
-    const { data } = await api.post('/students', body);
+    const { data } = await api.post('students', body);
     return data;
 }
 
 export async function getStudentRandom() {
-    const { data } = await api.get('/students/random');
+    const { data } = await api.get('students/random');
     return data;
 }
